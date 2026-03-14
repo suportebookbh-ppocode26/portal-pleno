61e343da931b8869d9cd9c4c75bbc59c14a40379
suportebookbh-ppocode26 <suportebookbh@gmail.com>
2026-03-14 20:30:02 -0300

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black border-t-2 border-[#FFD700] py-10 text-center">
-      <p className="text-[#AAA] text-[0.7rem] uppercase tracking-[2px]">
-        © 2026 Portal Pleno O Código | Todos os Direitos Reservados
-      </p>
-    </footer>
-  );
-}
